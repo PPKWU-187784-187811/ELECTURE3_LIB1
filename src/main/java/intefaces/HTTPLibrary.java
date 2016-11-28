2fc477693b58d5203f7aca000c0d7480343bb282
AdamPiech <adampiech7@op.pl>
2016-11-28 19:48:44 +0100

    Added downloadFile method to HTTP API.

--- src/main/java/intefaces/HTTPLibrary.java
+++ src/main/java/intefaces/HTTPLibrary.java
@@ -4,7 +4,8 @@ package intefaces;
  * Created by Adam Piech on 2016-11-22.
  */
 
-public interface HTTPLibrary{
+public interface HTTPLibrary {
 
-
+    public String downloadFile(String url);
+    
 }
