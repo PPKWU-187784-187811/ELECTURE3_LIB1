94aa8c974c5ccd2a8b26972cb9c4cdee1021f1a4
AdamPiech <adampiech7@op.pl>
2016-11-28 20:17:37 +0100

    Created  implementation HTTP APPI

---
+++ src/main/java/implementation/HTTPLibrary.java
@@ -0,0 +1,18 @@
+package implementation;
+
+import intefaces.IHTTPLibrary;
+
+import java.io.File;
+
+/**
+ * Created by Adam Piech on 2016-11-28.
+ */
+
+public class HTTPLibrary implements IHTTPLibrary {
+
+    @Override
+    public File downloadFile(String url) {
+        return null;
+    }
+
+}
