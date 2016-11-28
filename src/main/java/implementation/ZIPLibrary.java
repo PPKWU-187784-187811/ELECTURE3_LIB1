b7f45a3fee902780ab15c74fe2fdb24d42e421e4
AdamPiech <adampiech7@op.pl>
2016-11-28 20:47:55 +0100

    Created implementation ZIP API

---
+++ src/main/java/implementation/ZIPLibrary.java
@@ -0,0 +1,20 @@
+package implementation;
+
+import intefaces.IZIPLibrary;
+
+/**
+ * Created by Adam Piech on 2016-11-28.
+ */
+
+public class ZIPLibrary implements IZIPLibrary {
+
+    @Override
+    public void compress(String source, String destination, String password) {
+
+    }
+
+    @Override
+    public void decompress(String source, String destination, String password) {
+
+    }
+}
