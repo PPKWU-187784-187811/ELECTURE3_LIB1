77a91a78bb0d81312907ab9d944fae4aef75aa2e
AdamPiech <adampiech7@op.pl>
2016-11-28 20:32:28 +0100

    Reafactored HTTPLibrary

--- src/main/java/implementation/HTTPLibrary.java
+++ src/main/java/implementation/HTTPLibrary.java
@@ -13,6 +13,10 @@ import java.net.URL;
 
 public class HTTPLibrary implements IHTTPLibrary {
 
+    private static int BUFFER_SIZE = 1024;
+    private static int OFFSET = 0;
+    private static int END_OF_FILE = -1;
+
     @Override
     public void downloadFile(String url, String file) throws IOException {
         BufferedInputStream bufferStream = null;
@@ -20,17 +24,20 @@ public class HTTPLibrary implements IHTTPLibrary {
         try {
             bufferStream = new BufferedInputStream(new URL(url).openStream());
             fileStream = new FileOutputStream(file);
-
-            byte data[] = new byte[1024];
-            int count;
-            while ((count = bufferStream.read(data, 0, 1024)) != -1) {
-                fileStream.write(data, 0, count);
-            }
+            readData(bufferStream, fileStream);
         } finally {
             if (bufferStream != null)
                 bufferStream.close();
             if (fileStream != null)
                 fileStream.close();
+        }
+    }
+
+    private void readData(BufferedInputStream bufferStream, FileOutputStream fileStream) throws IOException {
+        byte data[] = new byte[BUFFER_SIZE];
+        int count;
+        while ((count = bufferStream.read(data, OFFSET, BUFFER_SIZE)) != END_OF_FILE) {
+            fileStream.write(data, OFFSET, count);
         }
     }
 
